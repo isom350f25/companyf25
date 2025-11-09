38df0ad459634b271860dc39737993ff26da1bcd
lailahaidar <145322841+lailahaidar@users.noreply.github.com>
2025-11-09 15:01:14 +0300

    completed

--- companyproj/compapp/models.py
+++ companyproj/compapp/models.py
@@ -11,3 +11,15 @@ class Employee(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.position}"
+    
+class Project(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, 
+                                 related_name='projects')
+    name = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2, 
+                                 blank=True, null=True )
+
+    def __str__(self):
+        return self.name
--- companyproj/compapp/urls.py
+++ companyproj/compapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('employeeslist/', views.employee_list, name='employee_list'),
+    path('employee/<int:employee_id>/', views.employee_detail, name='employee_detail'),
+    path('engineers/', views.employee_engineers, name='employee_engineers'),
 ]
